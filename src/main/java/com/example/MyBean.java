ae0fbe7c10c4a53b85368a9b947428d049a03103
Supun Dharmarathna <supun921@gmail.com>
2017-05-16 07:17:25 +0530

    Rest Template

---
+++ src/main/java/com/example/MyBean.java
@@ -0,0 +1,14 @@
+@Service
+public class MyBean {
+
+    private final RestTemplate restTemplate;
+
+    public MyBean(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public Details someRestCall(String name) {
+        return this.restTemplate.getForObject("/{name}/details", Details.class, name);
+    }
+
+}
